53746b0299902f1167b4f9f1941a2dd8404681bb
Jafagervik <jorgenafagervik@gmail.com>
2023-05-26 11:24:40 +0900

    sre

--- config.py
+++ config.py
@@ -18,7 +18,11 @@ LR = 1e-3
 IMG_SIZE = 28
 NUM_CHANNELS = 3
 MOMENTUM = 1e-3
+DROPOUT_RATE = 0.2
 
+TRAIN_RATIO = 0.65
+VALIDATE_RATIO = 0.15
+TEST_RATIO = 0.2
 
 # Directories
 TRAIN_DIR = "data/pizza_steak_sushi/train"
