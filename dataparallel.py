8ecc551e80abfb78538fd9b038fc88604378a233
Jafagervik <jorgenafagervik@gmail.com>
2023-06-24 16:10:12 +0900

    Example of vgg16 with parallel and transfer learning and everything

--- dataparallel.py
+++ dataparallel.py
@@ -8,87 +8,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 from torchvision import datasets, transforms
+import time
+import numpy as np 
+import matplotlib.pyplot as plt 
+from tqdm import tqdm
+from typing import List
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.rnn = nn.LSTM(input_size=28, hidden_size=64, batch_first=True)
-        self.batchnorm = nn.BatchNorm1d(64)
-        self.dropout1 = nn.Dropout2d(0.25)
-        self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(64, 32)
-        self.fc2 = nn.Linear(32, 10)
-
-    def forward(self, input):
-        # Shape of input is (batch_size,1, 28, 28)
-        # converting shape of input to (batch_size, 28, 28)
-        # as required by RNN when batch_first is set True
-        input = input.reshape(-1, 28, 28)
-        output, hidden = self.rnn(input)
-
-        # RNN output shape is (seq_len, batch, input_size)
-        # Get last output of RNN
-        output = output[:, -1, :]
-        output = self.batchnorm(output)
-        output = self.dropout1(output)
-        output = self.fc1(output)
-        output = F.relu(output)
-        output = self.dropout2(output)
-        output = self.fc2(output)
-        output = F.log_softmax(output, dim=1)
-        return output
-
-
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
-
-
-def test(args, model, device, test_loader, highest_acc: float):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-            if args.dry_run:
-                break
-
-    test_loss /= len(test_loader.dataset)
-
-    acc = 100. * correct / len(test_loader.dataset)
-
-    if acc > highest_acc:
-        highest_acc = acc 
-        torch.save(model.state_dict(), f"best_model.pt")
-
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
-    return highest_acc
-
-
-def main():
-    # Training settings
+def parse_args():
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example using RNN')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
@@ -110,37 +37,212 @@ def main():
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='for Saving the current Model')
-    parser.add_argument('--load-model', action='store_true', default=True,
+    parser.add_argument('--load-model', action='store_true', default=False,
                         help='for Loading the current Model')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+# def plotting(class_names, data, samples: int = 16):
+#     fig = plt.figure(figsize=(9, 9))
+#     n = np.sqrt(samples)
+#     rows, cols = n, n 
+#
+#     for i in range(1, rows * cols + 1):
+#         random_idx = torch.randint(0, len(train_data), size=[1]).item()
+#         img, label = train_data[random_idx]
+#         fig.add_subplot(rows, cols, i)
+#         plt.imshow(img.squeeze(), cmap="gray")
+#         plt.title(class_names[label])
+#         plt.axis(False);
+
+
+class ConvBlock(nn.Module):
+    """
+    https://learnopencv.com/understanding-convolutional-neural-networks-cnn/
+
+    This is a more concise version of this:
+    https://github.com/chongwar/vgg16-pytorch/blob/master/vgg16.py
+
+
+    Utilizing the power of reuse, we can easily set up a general block and 
+    just reuse this many times
+    """
+    def __init__(self, in_channels: int, out_channels: int, convs: int, kernel_size: int = 3, stride: int = 1, padding: int = 1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        # Batch_norm is here the same as out_channels, so we don't need to add an extra parameter
+        # See the github link for explenations
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+        self.a = nn.ReLU(True)
+        self.mp = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.convs = convs
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.batch_norm(x)
+        x = self.a(x)
+
+        # Some layers do 2 convolutions first, some do 3
+        for i in range(self.convs):
+            x = self.conv2(x)
+            x = self.batch_norm(x)
+            x = self.a(x)
+
+        x = self.mp(x)
+
+        return x
+
+
+class ClassifierLayer(nn.Module):
+    """
+    Classifier layer. this is actually redundant due to how little it does,
+    but it shows how you don't need to keep all nn info 
+    in a single class
+    """
+
+    def __init__(self, inp: int, out: int):
+        super().__init__()
+        self.dense = nn.Linear(inp, out) # DENSE LAYER is called Linear in pytorch
+        self.act = nn.ReLU(True)
+        self.drop = nn.Dropout(p=0.65)
+
+    def forward(self, x):
+        x = self.dense(x)
+        x = self.act(x)
+        x = self.drop(x)
+
+        return x
+
+
+class VGG16(nn.Module):
+    def __init__(self, channels: List[int]):
+        super().__init__()
+
+        assert(len(channels) == 9), "Dimension of channels does not match VGG16 net"
+
+        # conv_layers = [1, 64, 128, 256, 512, 512, 4096, 4096, 10]
+
+        self.conv1 = ConvBlock(in_channels=channels[0], out_channels=channels[1], convs=2)
+        self.conv2 = ConvBlock(in_channels=channels[1], out_channels=channels[2], convs=2)
+
+        self.conv3 = ConvBlock(in_channels=channels[2], out_channels=channels[3], convs=3)
+        self.conv4 = ConvBlock(in_channels=channels[3], out_channels=channels[4], convs=3)
+        self.conv5 = ConvBlock(in_channels=channels[4], out_channels=channels[5], convs=3)
+
+        self.classifier = nn.Sequential(
+            ClassifierLayer(channels[5], channels[6]),
+            ClassifierLayer(channels[6], channels[7]), 
+            nn.Linear(channels[7], channels[-1]), # DENSE LAYER
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
+                if m.bias is not None:
+                    m.bias.detach().zero_()
+
+        
+    def forward(self, x):
+        # FEATURE EXTRACTOR 
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        
+        # Flatten
+        x = x.view(x.size(0), -1) 
+        
+        # Classifier
+        x = self.classifier(x)
+        
+        # Softmax before output
+        x = F.softmax(x, dim=1) 
+
+        return x
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        # if batch_idx % args.log_interval == 0:
+        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        #         epoch, batch_idx * len(data), len(train_loader.dataset),
+        #                100. * batch_idx / len(train_loader), loss.item()))
+        #     if args.dry_run:
+        #         break
+
+
+@torch.no_grad()
+def test(args, model, device, test_loader, highest_acc: float):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        if args.dry_run:
+            break
+
+    test_loss /= len(test_loader.dataset)
+
+    acc = 100. * correct / len(test_loader.dataset)
+
+    if acc > highest_acc:
+        highest_acc = acc 
+        torch.save(model.state_dict(), f"best_model.pt")
+
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    return highest_acc
+
+
+def get_transforms():
+    """Set up some transforms. These 3 are fairly common"""
+    return transforms.Compose([
+        transforms.Resize((32,32)), # BAD
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+
+def main():
+    # Training settings
+    args = parse_args()
+
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
 
-    # print(torch.cuda.device_count())
-
     device = torch.device("cuda" if use_cuda else "cpu")
 
-
-    print(f"Device: {device}")
+    print(f"{device=}")
 
     for i in range(torch.cuda.device_count()):
         print(torch.cuda.get_device_properties(i))
 
+    transforms = get_transforms()
+
     kwargs = {'num_workers': 2, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.FashionMNIST('./data', train=True, download=True, transform=transforms),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data', train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])),
+        datasets.FashionMNIST('./data', train=False, transform=transforms),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     # ==========================================================================
@@ -148,11 +250,9 @@ def main():
     #       Dataparalllel 
     #
     # ==========================================================================
-    model = Net()
+    model = VGG16(channels=[1, 64, 128, 256, 512, 512, 4096, 4096, 10])
 
-    if use_cuda:
-        model = nn.DataParallel(model)
-    model.to(device)
+    model = nn.DataParallel(model)
 
     compiled = torch.compile(model)
 
@@ -167,13 +267,19 @@ def main():
     
     highest_acc = 0.0
 
+    start = time.time()
     for epoch in range(1, args.epochs + 1):
         train(args, compiled, device, train_loader, optimizer, epoch)
         highest_acc = test(args, compiled, device, test_loader, highest_acc)
         scheduler.step()
 
+    end = time.time()
+    elapsed = end - start
+
     if args.save_model:
-        torch.save(compiled.state_dict(), "mnist_rnn.pt")
+        torch.save(compiled.state_dict(), "mnist_vgg16.pt")
+
+    print(f"Elapsed time: {elapsed} seconds")
 
 
 if __name__ == '__main__':
